d524785f7ef305487ba66ab745cc0662752d2e87
Ridwansumardi <ridwanrizkia04@gmail.com>
2023-11-21 14:18:05 +0700

    Initial commit

---
+++ app/src/main/java/com/ridwan/pantipeduli/Loginandsignup.kt
@@ -0,0 +1,42 @@
+package com.ridwan.pantipeduli
+
+import android.app.Dialog
+import android.content.Intent
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import com.ridwan.pantipeduli.databinding.ActivityLoginandsignupBinding
+
+class Loginandsignup : AppCompatActivity() {
+    private lateinit var binding: ActivityLoginandsignupBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        binding = ActivityLoginandsignupBinding.inflate(layoutInflater)
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+        val btndaftar = findViewById<Button>(R.id.btn_daftar)
+        btndaftar.setOnClickListener {
+            val dialogBinding = layoutInflater.inflate(R.layout.daftaruser,null)
+
+            val myDialog = Dialog(this)
+            myDialog.setContentView(dialogBinding)
+
+            myDialog.setCancelable(true)
+            myDialog.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+            myDialog.show()
+        }
+
+        binding.imkiri1.setOnClickListener {
+            val i = Intent(this@Loginandsignup,Welcom::class.java)
+            startActivity(i)
+            finish()
+        }
+        binding.btnMasuk.setOnClickListener {
+            val i = Intent(this@Loginandsignup,Login::class.java)
+            startActivity(i)
+            finish()
+        }
+
+    }
+}
---
+++ app/src/main/java/com/ridwan/pantipeduli/Welcom.kt
@@ -0,0 +1,22 @@
+package com.ridwan.pantipeduli
+
+import android.content.Intent
+import android.media.session.MediaSession.Token
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import com.ridwan.pantipeduli.databinding.ActivityWelcomBinding
+
+class Welcom : AppCompatActivity() {
+    private lateinit var binding: ActivityWelcomBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        binding = ActivityWelcomBinding.inflate(layoutInflater)
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+        binding.btnMulai.setOnClickListener {
+            val x = Intent(this@Welcom,Loginandsignup::class.java)
+            startActivity(x)
+            finish()
+        }
+    }
+}
